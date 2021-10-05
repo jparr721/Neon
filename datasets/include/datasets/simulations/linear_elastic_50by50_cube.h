889b9fa4774f6f986d025867730610f63718fe1a
Jarred Parr <jparr721@gmail.com>
2021-10-05 11:11:55 -0600

    Preparing for first real dataset run, re-add granular details.

--- datasets/include/datasets/simulations/linear_elastic_50by50_cube.h
+++ datasets/include/datasets/simulations/linear_elastic_50by50_cube.h
@@ -23,32 +23,27 @@ namespace simulations::static_files {
     // Youngs Modulus
     constexpr int kMinE = 30000;
     constexpr int kMaxE = 150000;
-    // Changeme
-    constexpr int kEIncr = 10000;
+    constexpr int kEIncr = 1000;
 
     // Poisson's Ratio
     constexpr Real kMinv = 0.0;
     constexpr Real kMaxv = 0.5;
-    // Changeme
-    constexpr Real kvIncr = 0.1;
+    constexpr Real kvIncr = 0.01;
 
     // Shear Modulus
     constexpr Real kMinG = 1e-5;// Rubber
     constexpr Real kMaxG = 480; // Diamond
-    // Changeme
-    constexpr Real kGIncr = 10;
+    constexpr Real kGIncr = 1;
 
     // Thickness
     constexpr Real kMinT = 0.1;
     constexpr Real kMaxT = 0.7;
-    //Changeme
-    constexpr Real kTIncr = 0.1;
+    constexpr Real kTIncr = 0.01;
 
     // Amplitude
     constexpr Real kMinA = 0.1;
     constexpr Real kMaxA = 0.9;
-    // Changeme
-    constexpr Real kAIncr = 0.1;
+    constexpr Real kAIncr = 0.01;
 
 }// namespace simulations::static_files
 
